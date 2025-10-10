137e058f0cd5a4e0deb22ed68d853eb9d28580d4
Ian Dougherty <ianedougherty01@gmail.com>
2025-10-10 01:22:12 +0100

    render loop fixes

--- render_loop.py
+++ render_loop.py
@@ -1,11 +1,3 @@
-import sys
-import os
-picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'waveshare/pic')
-libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'waveshare/lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-
-from waveshare_epd import epd3in97
 from datetime import datetime, timedelta
 import time
 import threading
@@ -13,14 +5,14 @@ from draw import render_tamagotchi
 from mappings import get_current_action
 from tamagotchi import Tamagotchi
 
-def minute_loop(func):
+def minute_loop(func, arg):
     target_time = datetime.now().replace(second=0)
 
     while True:
         now = datetime.now()
 
         if now > target_time:
-            func()
+            func(arg)
             target_time = target_time + timedelta(minutes=1)
         else:
             timeout = (target_time - now).total_seconds()
@@ -28,21 +20,34 @@ def minute_loop(func):
             time.sleep(timeout)
 
 def launch_render_loop(tamagotchi):
-    epd = epd3in97.EPD()
 
-    def render():
+    def render(epd):
         now = datetime.now()
-        sprite = t.get_sprite()
-        quote = "Testing..."#t.generate_quote()
+        sprite = tamagotchi.get_sprite()
+        quote = "Testing..."#tamagotchi.generate_quote()
         print(now)
         action = get_current_action(now)
         print(action)
-        im = render_tamagotchi(t, sprite, None, quote)
+        im = render_tamagotchi(tamagotchi, sprite, None, quote)
         # im.show()
         epd.init()
         epd.display(epd.getbuffer(im))
 
-    minute_thread = threading.Thread(target=minute_loop, args=(render,))
+    def launch():
+        import sys
+        import os
+        picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'waveshare/pic')
+        libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'waveshare/lib')
+        if os.path.exists(libdir):
+            sys.path.append(libdir)
+	
+        from waveshare_epd import epd3in97
+        epd = epd3in97.EPD()
+
+        minute_loop(render, epd)
+
+
+    minute_thread = threading.Thread(target=launch)
     minute_thread.start()
 
 if __name__ == "__main__":
